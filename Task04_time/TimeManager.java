cbeec2cd9f7080243d43cd681df2a10037c27c12
annastakanova <50991493+annastakanova@users.noreply.github.com>
2019-10-15 03:15:58 +0300

    разделены логика и сущности

--- Task04_time/TimeManager.java
+++ Task04_time/TimeManager.java
@@ -1,85 +1,66 @@
 package by.epam.JavaEpam03_classes;
+import java.util.Objects;
 
 public class TimeManager {
     private int newHours;
     private int newMinutes;
     private int newSeconds;
 
-
-    public int get_hours() {
-        return newHours;
-    }
-    public void set_hours(int m_Hours) {
-        this.newHours = m_Hours;
-        Cansellation();
-    }
-    public int get_minutes() {
-        return newMinutes;
-    }
-    public void set_minutes(int m_Minutes) {
-        this.newMinutes = m_Minutes;
-        Cansellation();
-    }
-    public int get_seconds() {
-        return newSeconds;
-    }
-    public void set_seconds(int m_Seconds) {
-        this.newSeconds = m_Seconds;
-        Cansellation();
-    }
-
     public TimeManager() {
-        this.newHours = 0;
-        this.newMinutes = 0;
-        this.newSeconds = 0;
+        this.newHours = 20;
+        this.newMinutes = 55;
+        this.newSeconds = 45;
     }
 
     public TimeManager(int hours, int minutes, int seconds) {
-        if (Check_Time(hours, minutes, seconds) == true) {
-            this.newHours = hours;
-            this.newMinutes = minutes;
-            this.newSeconds = seconds;
-            Cansellation();
-        } else {
-            this.newHours = 0;
-            this.newMinutes = 0;
-            this.newSeconds = 0;
-            System.out.println("Неверные данные");
-        }
+        TimeManager tml = new TimeManager ();
+        this.newHours = hours;
+        this.newMinutes = minutes;
+        this.newSeconds = seconds;
     }
 
-    public void Change_Time(int hours, int minutes, int seconds) {
-        this.newHours += hours;
-        this.newMinutes += minutes;
-        this.newSeconds += seconds;
-        Cansellation();
-        if (Check_Time(hours, minutes, seconds) == false) {
-            this.newHours = 0;
-            this.newMinutes = 0;
-            this.newSeconds = 0;
-            System.out.println("Неверные данные");
-        }
+    public int getHours() {
+        return newHours;
+    }
+    public void setHours(int m_Hours) {
+        this.newHours = m_Hours;
     }
 
-    public boolean Check_Time(int hours, int minutes, int seconds) {
-        if (hours < 0 || hours > 24 || minutes < 0 || minutes > 59 || seconds < 0 || seconds > 59) {
-            return false;
-        }
-
-        return true;
+    public int getMinutes() {
+        return newMinutes;
+    }
+    public void setMinutes(int m_Minutes) {
+        this.newMinutes = m_Minutes;
     }
 
-    public void Cansellation() {
-        if (newSeconds > 59) {
-            newSeconds -= 60;
-            newMinutes++;
-        if (newMinutes > 59) {
-                newMinutes -= 60;
-                newHours++;
-            }
-        if (newHours > 23) {
-            newHours -= 24;
-            }
-        }
+    public int getSeconds() {
+        return newSeconds;
+    }
+    public void setSeconds(int m_Seconds) {
+        this.newSeconds = m_Seconds;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TimeManager that = (TimeManager) o;
+        return newHours == that.newHours &&
+                newMinutes == that.newMinutes &&
+                newSeconds == that.newSeconds;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(newHours, newMinutes, newSeconds);
+    }
+
+    @Override
+    public String toString() {
+        return "TimeManager{" +
+                "newHours=" + newHours +
+                ", newMinutes=" + newMinutes +
+                ", newSeconds=" + newSeconds +
+                '}';
     }
 }
